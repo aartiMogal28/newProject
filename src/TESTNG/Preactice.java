23240ea36dcf9ba3fd9a16a775465a0284b41b7e
aartiMogal28@ <aartimogal2002@gmail.com>
2025-01-02 15:08:59 +0530

    'created'

---
+++ src/TESTNG/Preactice.java
@@ -0,0 +1,39 @@
+package TESTNG;
+
+import org.testng.annotations.Test;
+
+import com.beust.testng.TestNG;
+
+public class Preactice {
+
+	
+	@Test
+
+	 public void abc()
+	 {
+		System .out.println("this is abc form");	
+		
+	 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        TestNG test=new TestNG();
+
+	
+	}
+
+}
